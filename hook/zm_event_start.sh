b0bca1d9e38d965c45deb924900c073b608b2bf3
sjiampojamarn <18257803+sjiampojamarn@users.noreply.github.com>
2023-10-21 23:38:41 -0700

    Detect camera 1 and 2 to trigger HOOK URL

--- hook/zm_event_start.sh
+++ hook/zm_event_start.sh
@@ -16,28 +16,22 @@ cleanup() {
 # $4 = cause of alarm 
 # $5 = path to event store (if store_frame_in_zm is 1)
 
-
-
 # Only tested with ZM 1.32.3+. May or may not work with older versions
 # Logic:
 # This script is invoked by zmeventnotification is you've specified its location in the hook= variable of zmeventnotification.pl
-
 
 # change this to the path of the object detection config"
 CONFIG_FILE="/etc/zm/objectconfig.ini"
 EVENT_PATH="$5"
 REASON="$4"
 
-
 # use arrays instead of strings to avoid quote hell
-if [[ ! -z "${2}" ]]
-then 
-   DETECTION_SCRIPT=(/var/lib/zmeventnotification/bin/zm_detect.py --monitorid $2 --eventid $1 --config "${CONFIG_FILE}" --eventpath "${EVENT_PATH}" --reason "${REASON}"  )
-else
-   DETECTION_SCRIPT=(/var/lib/zmeventnotification/bin/zm_detect.py  --eventid $1 --config "${CONFIG_FILE}" --eventpath "${EVENT_PATH}" --reason "${REASON}"  )
-
+DETECTION_SCRIPT=(/var/lib/zmeventnotification/bin/zm_detect.py --monitorid $2 --eventid $1 --config "${CONFIG_FILE}" --eventpath "${EVENT_PATH}" --reason "${REASON}")
+date
+if [ $2 == '1' ] || [ $2 == '2' ] ; then
+  echo ${DETECTION_SCRIPT[@]}
+  wget -O/dev/null -q ${ZM_NOTIFY_HOOK_URL}
 fi
-RESULTS=$("${DETECTION_SCRIPT[@]}" | grep "detected:")
 
 _RETVAL=1
 # The script needs  to return a 0 for success ( detected) or 1 for failure (not detected)
